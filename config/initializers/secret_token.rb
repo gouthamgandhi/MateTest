0c0acdac23a2f3bc3b0fb427f5b0a21c998fd13b
Goutham Gandhi Nadendla <nadendla@upenn.edu>
2011-09-20 17:38:02 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MateTest::Application.config.secret_token = '5d3794d5b8277d90f4bae26d6c9d970e404b9d12f052ff08254332c84454e5c25d8f4332f3bbec1b546170991a5966afb44d1c4e5b0e83d0389661a9383f60db'
